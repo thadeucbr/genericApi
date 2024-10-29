cd659c84c789c0d949436ad6557b3be70cd4b657
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-10-29 09:34:24 -0300

    Add ESLint configuration file and update package.json scripts

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+    ignorePatterns: ["tsconfig.json"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
